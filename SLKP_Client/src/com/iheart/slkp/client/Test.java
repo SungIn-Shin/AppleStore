5eea6011a25682684b7497e944719c81f4710191
SungIn <tlstjddls123@naver.com>
2015-09-18 17:48:16 +0900

    123

---
+++ SLKP_Client/src/com/iheart/slkp/client/Test.java
@@ -0,0 +1,11 @@
+package com.iheart.slkp.client;
+
+import java.math.BigInteger;
+
+public class Test {
+	public final static String PHARM_URL = "http://openapi.hira.or.kr/openapi/service/pharmacyInfoService/getParmacyBasisList";
+    public final static String KEY = "??????????????????";
+	public static void main(String[] args) {
+	
+	}
+}
